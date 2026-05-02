97f77cd7e9672e121e0145ae99420e6976964da7
Nayan <nayanjani120@gmail.com>
2026-05-03 01:09:18 +0530

    new file:   CRT-PHASE1/even.cpp

---
+++ CRT-PHASE1/even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"a=";
+    cin>>a;
+    if(a % 2 ==0){
+        cout<<a<<" is an even no."<<endl;
+
+    }
+    else{
+        cout<<a<<" is an odd no."<<endl;
+
+    }
+    return 0 ;
+}
+ 
