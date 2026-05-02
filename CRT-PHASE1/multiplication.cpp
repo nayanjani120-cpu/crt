3383dbb338c002757ceb1f81f23ed7ec4f9f7722
Nayan <nayanjani120@gmail.com>
2026-05-03 01:04:30 +0530

    new file:   CRT-PHASE1/multiplication.cpp

---
+++ CRT-PHASE1/multiplication.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"a=";
+    cin>>a;
+    for(int i=1; i<=10; i++){
+        cout<<a<<"X"<<i<<"="<<a*i<<endl;
+
+
+    }
+    return 0;
+}
