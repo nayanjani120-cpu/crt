09aeb1d077a5cd02935ee35f79d08a88bbac8c55
Nayan <nayanjani120@gmail.com>
2026-05-08 11:51:02 +0530

    normal commit

---
+++ CRT-PHASE1/day_8/static.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class employee{
+    public:
+    static int reg_no;
+
+    employee(){
+        reg_no++;
+    }
+
+    static void show(){
+        cout<<"employee reg no: "<<reg_no<<endl;
+    }
+};
+int employee::reg_no=100;
+
+
+int main(){
+    employee e1;
+    employee e2;
+    employee e3;
+    employee e4;
+    employee :: show();
+
+
+    return 0;
+}
